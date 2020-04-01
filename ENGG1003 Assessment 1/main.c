f06dde2a8233a9570ff65ad944f80d59fefbb367
Jack-C111 <jackcraven111@icloud.com>
2020-04-01 22:03:28 +1100

    1

--- ENGG1003 Assessment 1/main.c
+++ ENGG1003 Assessment 1/main.c
@@ -1,15 +1,8 @@
-//
-//  main.c
-//  ENGG1003 Assessment 1
-//
-//  Created by Jack Craven on 1/4/20.
-//  Copyright © 2020 Jack Craven. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    
+    
+    
     return 0;
 }
