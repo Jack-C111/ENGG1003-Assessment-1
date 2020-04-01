eb807d2b5eb5834c746e9a96fc0a88461cff97ad
Jack-C111 <jackcraven111@icloud.com>
2020-04-01 22:02:22 +1100

    Initial Commit

---
+++ ENGG1003 Assessment 1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ENGG1003 Assessment 1
+//
+//  Created by Jack Craven on 1/4/20.
+//  Copyright © 2020 Jack Craven. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
