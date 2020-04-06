c8af88085732c49c0d9763adc9ef3310ea85c82f
Jack-C111 <jackcraven111@icloud.com>
2020-04-06 21:57:27 +1000

    Menu begun

--- ENGG1003 Assessment 1/main.c
+++ ENGG1003 Assessment 1/main.c
@@ -1,8 +1,62 @@
 #include <stdio.h>
 
 int main() {
+    int menu_selection; //variable used to select from items on menu
+    FILE *RF_input, *RF_output;
     
-    printf("test_commit");
+    RF_input = fopen("Rail Fence Input", "r"); //opening rail fence input file in read mode
+    RF_output = fopen("Rail Fence Output", "w"); //opening rail fence output file in write mode
     
-    return 0;
+    printf("Select Task:\n\n(1) Rail Fence Encription\n(2) Exit\n\nEnter Selection: "); //menu text
+    scanf("%d", &menu_selection); //user input on main menu
+    
+    switch(menu_selection) { //menu
+        
+        case(1): //rail fence encription menu selection
+            printf("Rail Fence Encription Selected\n");
+            break;
+        
+        case(2): //exit program menu selection
+            printf("Program exited by user\n");
+            return 0;
+            break;
+    
+    }
+
 }
+
+
+
+
+
+/*
+
+int main() {
+    FILE *input;
+    FILE *output;
+
+    input = fopen("File", "r");
+    output = fopen("Result", "w");
+    
+    if (input == NULL) {
+        perror("error" );
+        return 1;
+    }
+    
+    char string, *p;
+    p = &string;
+    
+    while (!feof(input)) {
+        fscanf(input, "%c", p);
+        printf("%c", *p);
+        fprintf(output, "%c", *p);
+        
+        
+        
+    }
+
+    return 0;
+
+}
+
+*/
